eeb8e04e6c8de7ce9dcc3146e831c1fff98e2c0d
Anees-Alam <anees.rahman@ontariotechu.net>
2023-11-30 19:12:51 -0500

    login and register 1st commit

--- server/routes/Bio_books.js
+++ server/routes/Bio_books.js
@@ -3,17 +3,26 @@ var router = express.Router();
 //const { router } = require('../config/app');
 let Book = require('../models/Bio_books');
 let BookController = require('../controllers/Bio_books')
+let mongoose = require('mongoose');
+// helper function
+function requireAuth(req,res,next){
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login')
+    }
+    next();
+}
 /* Get route for the Bio Books list */
 // Read Operation
 router.get('/', BookController.DislayBooklist);
 /* Get route for Add Book page --> Create */
-router.get('/add', BookController.AddBook); 
+router.get('/add', requireAuth, BookController.AddBook); 
 /* Post route for Add Book page --> Create */
-router.post('/add', BookController.ProcessBook);
+router.post('/add', requireAuth, BookController.ProcessBook);
 /* Get route for displaying the Edit Book page --> Update */
-router.get('/edit/:id', BookController.EditBook);
+router.get('/edit/:id', requireAuth, BookController.EditBook);
 /* Post route for processing the Edit Book page --> Update */
-router.post('/edit/:id', BookController.ProcessEditBook);
+router.post('/edit/:id', requireAuth, BookController.ProcessEditBook);
 /* Get to perform Delete Operation --> Delete Operation */
-router.get('/delete/:id', BookController.DeleteBook);
+router.get('/delete/:id', requireAuth, BookController.DeleteBook);
  module.exports = router;
